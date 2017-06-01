a80ffff108d628227648fb6bac8414a4dcbb1a3f
Kjell-PC <dijkk009@hva.nl>
2017-06-01 11:32:46 +0200

    tryout camara
    ik probeer een leuke camara functie te hadde, werkt nog niet helemaal

--- SpanceInvadersUltra/SpaceShipGeneral.cs
+++ SpanceInvadersUltra/SpaceShipGeneral.cs
@@ -19,25 +19,43 @@ namespace SpanceInvadersUltra
         protected float speed = 300;
         protected float minSpeed = -300;
         protected float rotation;
+        protected bool shieldUsed = false;
 
         public SpaceShipGeneral() : base("spaceship")
         {
-            position = new Vector2(Main.Screen.X / 2, Main.Screen.Y / 2);     
+            position = new Vector2(Main.Screen.X / 2, Main.Screen.Y / 2);
+            origin = new Vector2(Width / 2, Height / 2);
         }
 
         public override void HandleInput(InputHelper inputHelper)
         {
+            //movement
             velocity = Vector2.Zero;
             if (inputHelper.IsKeyDown(Keys.Left))
                 velocity.X = minSpeed;
-            else if (inputHelper.IsKeyDown(Keys.Right))
+            if (inputHelper.IsKeyDown(Keys.Right))
                 velocity.X = speed;
-            else if (inputHelper.IsKeyDown(Keys.Up))
+            if (inputHelper.IsKeyDown(Keys.Up))
                 velocity.Y = minSpeed;
-            else if (inputHelper.IsKeyDown(Keys.Down))
-                velocity.Y = speed;    
+            if (inputHelper.IsKeyDown(Keys.Down))
+                velocity.Y = speed;
+
+            //shield key
+            if (inputHelper.IsKeyDown(Keys.Z))
+                shieldUsed = true;
+            else shieldUsed = false;
+            
             
             base.HandleInput(inputHelper);
+        }
+
+        protected void Stats()
+        {
+            fireRate = 20;
+            shield = 20;
+            health = 200;
+            armour = 100;
+            bullets = 50;
         }
 
         public override void Update(GameTime gameTime)
@@ -56,6 +74,7 @@ namespace SpanceInvadersUltra
             if (!visible || sprite == null)
                 return;
 
+            //for the rotation, need to fix the scale.
             //spriteBatch.Draw(sprite.Sprite, GlobalPosition, null, Color.White, rotation, origin, scale, SpriteEffects.None, 0.0f);
             base.Draw(gameTime, spriteBatch);
         }
