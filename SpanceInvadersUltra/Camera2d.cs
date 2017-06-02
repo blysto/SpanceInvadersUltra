fb18fd2b98646f873fd66c34438931e3ba66367b
Kjell-PC <dijkk009@hva.nl>
2017-06-02 16:51:34 +0200

    Camara class 2.0
    Changed the camara class. But this still isnt final proboly will change it

--- SpanceInvadersUltra/Camera2d.cs
+++ SpanceInvadersUltra/Camera2d.cs
@@ -1,171 +1,146 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SpanceInvadersUltra
 {
-    public interface IFocusable
+    public class Camera2D
     {
-        Vector2 Position { get; }
-    }
 
-    public interface ICamera2D
-    {
-        /// <summary>
-        /// Gets or sets the position of the camera
-        /// </summary>
-        /// <value>The position.</value>
-        Vector2 Position { get; set; }
+        #region Fields
 
-        /// <summary>
-        /// Gets or sets the move speed of the camera.
-        /// The camera will tween to its destination.
-        /// </summary>
-        /// <value>The move speed.</value>
-        float MoveSpeed { get; set; }
-
-        /// <summary>
-        /// Gets or sets the rotation of the camera.
-        /// </summary>
-        /// <value>The rotation.</value>
-        float Rotation { get; set; }
-
-        /// <summary>
-        /// Gets the origin of the viewport (accounts for Scale)
-        /// </summary>        
-        /// <value>The origin.</value>
-        Vector2 Origin { get; }
-
-        /// <summary>
-        /// Gets or sets the scale of the Camera
-        /// </summary>
-        /// <value>The scale.</value>
-        float Scale { get; set; }
-
-        /// <summary>
-        /// Gets the screen center (does not account for Scale)
-        /// </summary>
-        /// <value>The screen center.</value>
-        Vector2 ScreenCenter { get; }
-
-        /// <summary>
-        /// Gets the transform that can be applied to 
-        /// the SpriteBatch Class.
-        /// </summary>
-        /// <see cref="SpriteBatch"/>
-        /// <value>The transform.</value>
-        Matrix Transform { get; }
-
-        /// <summary>
-        /// Gets or sets the focus of the Camera.
-        /// </summary>
-        /// <seealso cref="IFocusable"/>
-        /// <value>The focus.</value>
-        IFocusable Focus { get; set; }
-
-        /// <summary>
-        /// Determines whether the target is in view given the specified position.
-        /// This can be used to increase performance by not drawing objects
-        /// directly in the viewport
-        /// </summary>
-        /// <param name="position">The position.</param>
-        /// <param name="texture">The texture.</param>
-        /// <returns>
-        ///     <c>true</c> if the target is in view at the specified position; otherwise, <c>false</c>.
-        /// </returns>
-        bool IsInView(Vector2 position, Texture2D texture);
-    }
-
-    public class Camera2D : GameComponent, ICamera2D
-    {
-        private Vector2 _position;
-        protected float _viewportHeight;
-        protected float _viewportWidth;
-
-        public Camera2D(Game game)
-            : base(game)
-        { }
-
-        #region Properties
-
-        public Vector2 Position
-        {
-            get { return _position; }
-            set { _position = value; }
-        }
-        public float Rotation { get; set; }
-        public Vector2 Origin { get; set; }
-        public float Scale { get; set; }
-        public Vector2 ScreenCenter { get; protected set; }
-        public Matrix Transform { get; set; }
-        public IFocusable Focus { get; set; }
-        public float MoveSpeed { get; set; }
+        protected float _zoom; //Camera Zoom Value
+        protected Matrix _transform; //Camera Transform Matrix
+        protected Matrix _inverseTransform; //Inverse of Transform Matrix
+        protected Vector2 _pos; //Camera Position
+        protected float _rotation; //Camera Rotation Value (Radians)
+        protected Viewport _viewport; //Cameras Viewport
+        protected MouseState _mState; //Mouse state
+        protected KeyboardState _keyState; //Keyboard state
+        protected Int32 _scroll; //Previous Mouse Scroll Wheel Value
 
         #endregion
 
+        #region properties
+
+        public float Zoom
+        {
+            get { return _zoom; }
+            set { _zoom = value; }
+        }
         /// <summary>
-        /// Called when the GameComponent needs to be initialized. 
+        /// Camera View Matrix Property
         /// </summary>
-        public override void Initialize()
+        public Matrix Transform
         {
-            _viewportWidth = Game.GraphicsDevice.Viewport.Width;
-            _viewportHeight = Game.GraphicsDevice.Viewport.Height;
-
-            ScreenCenter = new Vector2(_viewportWidth / 2, _viewportHeight / 2);
-            Scale = 1;
-            MoveSpeed = 1.25f;
-
-            base.Initialize();
+            get { return _transform; }
+            set { _transform = value; }
         }
-
-        public override void Update(GameTime gameTime)
-        {
-            // Create the Transform used by any
-            // spritebatch process
-            Transform = Matrix.Identity *
-                        Matrix.CreateTranslation(-Position.X, -Position.Y, 0) *
-                        Matrix.CreateRotationZ(Rotation) *
-                        Matrix.CreateTranslation(Origin.X, Origin.Y, 0) *
-                        Matrix.CreateScale(new Vector3(Scale, Scale, Scale));
-
-            Origin = ScreenCenter / Scale;
-
-            // Move the Camera to the position that it needs to go
-            var delta = (float)gameTime.ElapsedGameTime.TotalSeconds;
-
-            _position.X += (Focus.Position.X - Position.X) * MoveSpeed * delta;
-            _position.Y += (Focus.Position.Y - Position.Y) * MoveSpeed * delta;
-
-            base.Update(gameTime);
-        }
-
         /// <summary>
-        /// Determines whether the target is in view given the specified position.
-        /// This can be used to increase performance by not drawing objects
-        /// directly in the viewport
+        /// Inverse of the view matrix, can be used to get objects screen coordinates
+        /// from its object coordinates
         /// </summary>
-        /// <param name="position">The position.</param>
-        /// <param name="texture">The texture.</param>
-        /// <returns>
-        ///     <c>true</c> if [is in view] [the specified position]; otherwise, <c>false</c>.
-        /// </returns>
-        public bool IsInView(Vector2 position, Texture2D texture)
+        public Matrix InverseTransform
         {
-            // If the object is not within the horizontal bounds of the screen
-
-            if ((position.X + texture.Width) < (Position.X - Origin.X) || (position.X) > (Position.X + Origin.X))
-                return false;
-
-            // If the object is not within the vertical bounds of the screen
-            if ((position.Y + texture.Height) < (Position.Y - Origin.Y) || (position.Y) > (Position.Y + Origin.Y))
-                return false;
-
-            // In View
-            return true;
+            get { return _inverseTransform; }
         }
+        public Vector2 Pos
+        {
+            get { return _pos; }
+            set { _pos = value; }
+        }
+        public float Rotation
+        {
+            get { return _rotation; }
+            set { _rotation = value; }
+        }
+
+        #endregion
+
+        #region Constructor
+
+        public Camera2D(Viewport viewport)
+        {
+            _zoom = 1.0f;
+            _scroll = 1;
+            _rotation = 0.0f;
+            _pos = Vector2.Zero;
+            _viewport = viewport;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void Update(GameTime gameTime)
+        {
+            //Call camera input
+            Input();
+            //Clamp zoom value
+            MathHelper.Clamp(_zoom, 0.01f, 10.0f);
+            //Clamp rotation value
+            _rotation = ClampAngle(_rotation);
+            //create view  matrix
+            _transform = Matrix.CreateRotationZ(_rotation) *
+                         Matrix.CreateScale(new Vector3(_zoom, _zoom, 1)) *
+                         Matrix.CreateTranslation(_pos.X, _pos.Y, 0);
+            //update inverse matrix
+            _inverseTransform = Matrix.Invert(_transform);
+        }
+
+        /// <summery>
+        /// Example Input Method, rotates using cursor keys and zooms using mouse wheel, I want to make one of my own, but have to find out how to do this first
+        /// </summery>
+        protected virtual void Input()
+        {
+            _mState = Mouse.GetState();
+            _keyState = Keyboard.GetState();
+            //check zoom
+            if (_mState.ScrollWheelValue > _scroll)
+            {
+                _zoom += 0.1f;
+                _scroll = _mState.ScrollWheelValue;
+            }
+            else if (_mState.ScrollWheelValue < _scroll)
+            {
+                _zoom -= 0.1f;
+                _scroll = _mState.ScrollWheelValue;
+            }
+            //check rotation
+            if (_keyState.IsKeyDown(Keys.Left))
+                _rotation -= 0.1f;
+            if (_keyState.IsKeyDown(Keys.Right))
+                _rotation += 0.1f;
+            //check move
+            if (_keyState.IsKeyDown(Keys.A))
+                _pos.X += 0.5f;
+            if (_keyState.IsKeyDown(Keys.D))
+                _pos.X -= 0.5f;
+            if (_keyState.IsKeyDown(Keys.W))
+                _pos.Y += 0.5f;
+            if (_keyState.IsKeyDown(Keys.S))
+                _pos.Y -= 0.5f;
+        }
+
+        ///<summery>
+        ///Clamps a radian value between -pi and pi
+        ///</summery>
+        ///<param name="radians">angle to be clamped </param>
+        ///<returns>clamped angle</returns>
+        protected float ClampAngle(float radians)
+        {
+            while (radians < -MathHelper.Pi)
+            {
+                radians += MathHelper.TwoPi;
+            }
+            while (radians > MathHelper.Pi)
+            {
+                radians -= MathHelper.TwoPi;
+            }
+            return radians;
+        }
+
+        #endregion
     }
 }
